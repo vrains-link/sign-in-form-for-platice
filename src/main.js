36dc5a7ddeeed521f9ee9d5ec2b079ab17cb1ed4
vrains-link <11030185@fhsh.tp.edu.tw>
2022-05-13 00:10:16 +0800

    add jqery and prevent default of form events

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-import './index.css'
+import "./index.css";
+
+$(document).ready(function () {
+  //原本純寫 Javascript 要打的指令
+  const form = document.getElementById("sign-in-form");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+  });
+
+  //jQuery 打的指令
+  $(#"sign-in-form").sumbit(function (e) {
+      e.preventDefault();
+  });
+});
